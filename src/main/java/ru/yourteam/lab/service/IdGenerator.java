cf3cc078794a8fb2c5c956bc4917692010e30fe6
matthewvaulin <matthewvaulin@gmail.com>
2026-04-04 04:03:33 +0500

    Add ID generator service

---
+++ src/main/java/ru/yourteam/lab/service/IdGenerator.java
@@ -0,0 +1,22 @@
+package ru.yourteam.lab.service;
+
+public final class IdGenerator {
+    private static long sampleIdCounter = 1;
+    private static long measurementIdCounter = 1;
+    private static long protocolIdCounter = 1;
+
+    private IdGenerator() {
+    }
+
+    public static synchronized long nextSampleId() {
+        return sampleIdCounter++;
+    }
+
+    public static synchronized long nextMeasurementId() {
+        return measurementIdCounter++;
+    }
+
+    public static synchronized long nextProtocolId() {
+        return protocolIdCounter++;
+    }
+}
