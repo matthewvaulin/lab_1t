d3b0a9bdef54717d82ab89b1fbf18d5cee99eb31
matthewvaulin <matthewvaulin@gmail.com>
2026-04-04 11:37:39 +0500

    Add temporary lab.Main entry point

---
+++ src/main/java/lab/Main.java
@@ -0,0 +1,10 @@
+package lab;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Temporary entry point");
+    }
+}
